474127c62c97c4cbe71672e1a19bda8a41d5ac8f
Neaga Ioana <ioanaandreea.neaga@ulbsibiu.ro>
2025-04-06 15:15:05 +0300

    Refacere tabel Colectari

---
+++ colectare-deseuri/colectare-deseuri/Migrations/20250406121340_FixForeignKey.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace colectare_deseuri.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixForeignKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Colectari_Pubele_PubelaId",
+                table: "Colectari");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Colectari_PubelaId",
+                table: "Colectari");
+
+            migrationBuilder.DropColumn(
+                name: "PubelaId",
+                table: "Colectari");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Colectari_IdPubela",
+                table: "Colectari",
+                column: "IdPubela");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Colectari_Pubele_IdPubela",
+                table: "Colectari",
+                column: "IdPubela",
+                principalTable: "Pubele",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Colectari_Pubele_IdPubela",
+                table: "Colectari");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Colectari_IdPubela",
+                table: "Colectari");
+
+            migrationBuilder.AddColumn<int>(
+                name: "PubelaId",
+                table: "Colectari",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Colectari_PubelaId",
+                table: "Colectari",
+                column: "PubelaId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Colectari_Pubele_PubelaId",
+                table: "Colectari",
+                column: "PubelaId",
+                principalTable: "Pubele",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- colectare-deseuri/colectare-deseuri/Models/Colectare.cs
+++ colectare-deseuri/colectare-deseuri/Models/Colectare.cs
@@ -1,10 +1,15 @@
-﻿namespace colectare_deseuri.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace colectare_deseuri.Models
 {
     public class Colectare
     {
         public int Id { get; set; }
         public int IdPubela { get; set; }
+
+        [ForeignKey("IdPubela")]
         public Pubela Pubela { get; set; }
+
         public DateTime TimpRidicare { get; set; }
         public required string Adresa { get; set; }
 
